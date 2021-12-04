c95f80527f1ab8fba184e348a69cbe0e38ccca29
Paul I Ighofose <paul@ipi-international.co.uk>
2021-12-04 16:16:23 +0000

    Learning nodeJs

---
+++ Develop a Web App/myapp/routes/users.js
@@ -0,0 +1,10 @@
+const global = require('../globals');
+var express = require(global + 'express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
