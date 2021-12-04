f66b18701ca9bab80aca4f0335c83da61606f6ed
Paul I Ighofose <paul@ipi-international.co.uk>
2021-12-04 16:17:22 +0000

    Learning nodeJs

---
+++ Develop a Web App/myapp/validators/edit_lead.js
@@ -0,0 +1,32 @@
+
+const global = require('../globals');
+let models = require('../models');
+let validator = require(global + 'validator');
+
+
+const validateCreateLead = function(errors, req) {
+	if (!validator.isEmail(req.body.lead_email)) {
+		errors['message'] = 'Please use a valid email.';
+		errors['email'] = req.body.lead_email;
+	}
+}
+
+exports.validateEditLead = function(errors, req) {
+	return new Promise(function(resolve, reject){
+		validateCreateLead(errors, req);
+		return models.Lead.findAll({
+			where: {
+				email: req.body.lead_email
+			}
+		}).then(e => {
+			if (e !== null && e.length > 1) {
+				errors['message'] = 'Email is already in use.';		
+				errors['email'] = req.body.lead_email;	
+			} else if (e !== null && e.length > 0 && e[0].dataValues.id !== req.params.lead_id) {
+				errors['message'] = 'Email is already in use.';		
+				errors['email'] = req.body.lead_email;					
+			}
+			resolve(errors);
+		});		
+	});
+}
---
+++ Develop a Web App/myapp/validators/lead.js
@@ -0,0 +1,29 @@
+
+const global = require('../globals');
+let models = require('../models');
+let validator = require(global + 'validator');
+
+
+const validateCreateLead = function(errors, req) {
+	if (!validator.isEmail(req.body.lead_email)) {
+		errors['message'] = 'Please use a valid email.';
+		errors['email'] = req.body.lead_email;
+	}
+}
+
+exports.validateLead = function(errors, req) {
+	return new Promise(function(resolve, reject){
+		validateCreateLead(errors, req);
+		return models.Lead.findOne({
+			where: {
+				email: req.body.lead_email
+			}
+		}).then(e => {
+			if (e !== null) {
+				errors['message'] = 'Email is already in use.';		
+				errors['email'] = req.body.lead_email;	
+			}
+			resolve(errors);
+		});		
+	});
+}
