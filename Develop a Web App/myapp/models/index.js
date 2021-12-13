95b2c1a6ff25e5ee3b44babc801ac5ef02db19b7
Paul I Ighofose <paul@ipi-international.co.uk>
2021-12-13 01:56:04 +0000

    Learning nodeJs

--- Develop a Web App/myapp/models/index.js
+++ Develop a Web App/myapp/models/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const global = require('../globals');
+const globalLoc = require('../globals');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require(global + 'sequelize');
+const Sequelize = require(globalLoc + 'sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
