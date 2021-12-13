b7912c2e122a6c85c7cde2d1e10136374faa9c87
Paul I Ighofose <paul@ipi-international.co.uk>
2021-12-13 01:49:25 +0000

    Learning nodeJs

--- Build a REST API/src/routes/person.js
+++ Build a REST API/src/routes/person.js
@@ -1,5 +1,5 @@
-const global = require('child_process').execSync('npm root -g').toString().trim().replace(/\\/g, '\\\\') + '\\\\';
-let express = require(global + 'express');
+const globalLoc = require('child_process').execSync('npm root -g').toString().trim().replace(/\\/g, '\\\\') + '\\\\';
+let express = require(globalLoc + 'express');
 let router = express.Router();
 
 // QuerString => query property on the request object
