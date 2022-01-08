7d5e871c7d0beb884b5af493ce328c6154efb8b6
Paul I Ighofose <paul@ipi-international.co.uk>
2022-01-08 14:01:33 +0000

    Learning nodeJs from Video Tutorials and books

---
+++ NodeJs Full Course for Beginners/chapter_15/middleware/logEvents.js
@@ -0,0 +1,29 @@
+const { globalLoc } = require('../../globals');
+const { format } = require(globalLoc + 'date-fns');
+const { v4: uuid } = require(globalLoc + 'uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises; // why did you not do fs.promises?
+const path = require('path');
+
+const logEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+      await fs.promises.mkdir(path.join(__dirname, '..', 'logs'));
+    }
+    await fs.promises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+  console.log(`${req.method} ${req.path}`);
+  next();
+}
+
+module.exports = { logger, logEvents };
